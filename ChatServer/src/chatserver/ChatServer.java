a044eafad939980cb2504d80b4899bfad282e8a5
tobi <tobi@bla.de>
2012-11-05 12:47:21 +0100

    Struktur für NB Projekte angepasst

---
+++ ChatServer/src/chatserver/ChatServer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatserver;
+
+/**
+ *
+ * @author Tobi
+ */
+public class ChatServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
