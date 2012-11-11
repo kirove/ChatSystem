c89469f97872ca27ad00e9ff8a26e19292835e3a
iBot <Tobi>
2012-11-11 17:29:04 +0100

    Fehlerkorrektur

--- ChatClient/src/chatclient/UDPServer.java
+++ ChatClient/src/chatclient/UDPServer.java
@@ -18,7 +18,8 @@ import java.util.concurrent.BlockingQueue;
 public class UDPServer {
 
     private static final int BUFFER_SIZE = 123;
-    private DatagramSocket clientSocket;  // UDP-Socketklasse
+    private final static int SERVER_PORT = 50001;
+    private DatagramSocket serverSocket;  // UDP-Socketklasse
     private final BlockingQueue<String> messageQueue;
     private boolean serviceRequested = true;
 
@@ -31,17 +32,19 @@ public class UDPServer {
         try {
             /* UDP-Socket erzeugen (kein Verbindungsaufbau!)
              * Socket wird an irgendeinen freien (Quell-)Port gebunden, da kein Port angegeben */
-            this.clientSocket = new DatagramSocket();
-
+            this.serverSocket = new DatagramSocket(SERVER_PORT);
+            System.out.println("UDP Server: Waiting for connection - listening UDP port " +
+                           SERVER_PORT);
+            
             String message;
 
             while (serviceRequested) {
-                message = readFromServer();
+                message = readFromClient();
                 this.messageQueue.add(message);
             }
 
             /* Socket schließen (freigeben)*/
-            clientSocket.close();
+            serverSocket.close();
         } catch (IOException e) {
             System.err.println(e.toString());
             System.exit(1);
@@ -53,7 +56,7 @@ public class UDPServer {
         this.serviceRequested = false;
     }
 
-    private String readFromServer() {
+    private String readFromClient() {
         /* Liefere den nächsten String vom Server */
         String receiveString = "";
 
@@ -63,15 +66,15 @@ public class UDPServer {
             DatagramPacket receivePacket = new DatagramPacket(receiveData, BUFFER_SIZE);
 
             /* Warte auf Empfang des Antwort-Pakets auf dem eigenen Port */
-            clientSocket.receive(receivePacket);
+            serverSocket.receive(receivePacket);
 
             /* Paket wurde empfangen --> auspacken und Inhalt anzeigen */
             receiveString = new String(receivePacket.getData(), 0, receivePacket.getLength());
         } catch (IOException e) {
-            System.err.println("Connection aborted by server!");
+            System.err.println("Connection aborted by client!");
             serviceRequested = false;
         }
-        System.out.println("UDP Client got from Server: " + receiveString);
+        System.out.println("UDP Client got from Client: " + receiveString);
         return receiveString;
     }
 }
